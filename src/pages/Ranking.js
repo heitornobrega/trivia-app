8078e9878818db7633d8ecfc13f1923b0c330046
Marcelo Carvalho <marcelo.carvalho2@outlook.com>
2022-06-30 18:29:42 -0300

    Requisito 18

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends Component {
-  render() {
-    return (
-      <h1
-        data-testid="ranking-title"
-      >
-        Ranking
-      </h1>
-    );
-  }
+    newGame = () => {
+      const { history } = this.props;
+      history.push('/');
+    };
+
+    render() {
+      return (
+        <>
+          <h1
+            data-testid="ranking-title"
+          >
+            Ranking
+          </h1>
+          <button
+            data-testid="btn-go-home"
+            type="button"
+            onClick={ this.newGame }
+          >
+            Go Home
+          </button>
+        </>
+      );
+    }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Ranking;
