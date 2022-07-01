cb97cbf1a09bbeb012a7647d4389df67e58d1238
heitornobrega <nobheitor@gmail.com>
2022-07-01 19:56:25 -0300

    requisito 4

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,7 +1,7 @@
 import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from '../reducer';
+import rootReducer from '../reducers';
 
 const store = createStore(
   rootReducer,
